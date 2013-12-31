b73c0624cd8f5910f412dd9720baec771ac1e62d
drakes8of8 <steve8of8@gmail.com>
2013-12-31 11:01:47 -0500

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Steven on 12/31/13.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
