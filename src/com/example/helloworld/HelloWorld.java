d4c9f3f6118cdcfed9b42b0ed7bf88de6e612cfc
drakes8of8 <steve8of8@gmail.com>
2014-01-02 08:40:41 -0500

    Initial commit

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,9 +2,10 @@ package com.example.helloworld;
 
 /**
  * Created by Steven on 12/31/13.
+ * Lets see if this text flows through GIT to TC from IDE
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Did This Work?");
     }
 }
